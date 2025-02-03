3aade400d449ca9bde52a5f201d43ae8caa7303e
Behzad <mirbehzad2004@gmail.com>
2025-02-03 23:41:42 +0330

    posts added to index.html

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include 'include/layout/header.php';
                 <?php
 
                 include 'include/layout/slider.php';
-
+                $posts = $connection->query("SELECT * FROM posts");
                 ?>
 
                 <!-- Content Section -->
@@ -18,153 +18,61 @@ include 'include/layout/header.php';
                         <!-- Posts Content -->
                         <div class="col-lg-8">
                             <div class="row g-3">
-                                <div class="col-sm-6">
-                                    <div class="card">
-                                        <img
-                                            src="./assets/images/4.jpg"
-                                            class="card-img-top"
-                                            alt="post-image"
-                                        />
-                                        <div class="card-body">
-                                            <div
-                                                class="d-flex justify-content-between"
-                                            >
-                                                <h5 class="card-title fw-bold">
-                                                    لورم ایپسوم
-                                                </h5>
-                                                <div>
-                                                    <span
-                                                        class="badge text-bg-secondary"
-                                                        >طبیعت</span
-                                                    >
-                                                </div>
-                                            </div>
-                                            <p
-                                                class="card-text text-secondary pt-3"
-                                            >
-                                                لورم ایپسوم متن ساختگی با تولید
-                                                سادگی نامفهوم از صنعت چاپ و با
-                                                استفاده از طراحان گرافیک است.
-                                                چاپگرها و متون بلکه روزنامه و
-                                                مجله در ستون و سطرآنچنان که لازم
-                                                است و برای شرایط فعلی تکنولوژی
-                                                مورد نیاز و کاربردهای متنوع با
-                                                هدف بهبود
-                                            </p>
-                                            <div
-                                                class="d-flex justify-content-between align-items-center"
-                                            >
-                                                <a
-                                                    href="single.html"
-                                                    class="btn btn-sm btn-dark"
-                                                    >مشاهده</a
-                                                >
+                                <?php if($posts->num_rows > 0){
+                                     foreach($posts as $post):
+                                        $categoryId = $post['category_id'];
+                                        $categories = $connection->query("SELECT* FROM categories WHERE id = $categoryId"); ?>
+                                            <div class="col-sm-6">
+                                                <div class="card">
+                                                    <img
+                                                        src="./uploads/posts/<?= $post['image']; ?>"
+                                                        class="card-img-top"
+                                                        alt="post-image"
+                                                    />
+                                                    <div class="card-body">
+                                                        <div
+                                                            class="d-flex justify-content-between"
+                                                        >
+                                                            <h5 class="card-title fw-bold">
+                                                                <?= $post['title']; ?>
+                                                            </h5>
+                                                            <div>
+                                                                <span
+                                                                    class="badge text-bg-secondary"
+                                                                    ><?= $categories->fetch_assoc()['title']; ?></span
+                                                                >
+                                                            </div>
+                                                        </div>
+                                                        <p
+                                                            class="card-text text-secondary pt-3"
+                                                        >
+                                                            <?= substr($post['body'],0,200) . "..."; ?>
+                                                        </p>
+                                                        <div
+                                                            class="d-flex justify-content-between align-items-center"
+                                                        >
+                                                            <a
+                                                                href="single.html"
+                                                                class="btn btn-sm btn-dark"
+                                                                >مشاهده</a
+                                                            >
 
-                                                <p class="fs-7 mb-0">
-                                                    نویسنده : علی شیخ
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="card">
-                                        <img
-                                            src="./assets/images/5.jpg"
-                                            class="card-img-top"
-                                            alt="post-image"
-                                        />
-                                        <div class="card-body">
-                                            <div
-                                                class="d-flex justify-content-between"
-                                            >
-                                                <h5 class="card-title fw-bold">
-                                                    لورم ایپسوم
-                                                </h5>
-                                                <div>
-                                                    <span
-                                                        class="badge text-bg-secondary"
-                                                        >گردشگری</span
-                                                    >
+                                                            <p class="fs-7 mb-0">
+                                                                نویسنده :  <?= $post['author']; ?>
+                                                            </p>
+                                                        </div>
+                                                    </div>
                                                 </div>
+                                                
                                             </div>
-                                            <p
-                                                class="card-text text-secondary pt-3"
-                                            >
-                                                لورم ایپسوم متن ساختگی با تولید
-                                                سادگی نامفهوم از صنعت چاپ و با
-                                                استفاده از طراحان گرافیک است.
-                                                چاپگرها و متون بلکه روزنامه و
-                                                مجله در ستون و سطرآنچنان که لازم
-                                                است و برای شرایط فعلی تکنولوژی
-                                                مورد نیاز و کاربردهای متنوع با
-                                                هدف بهبود
-                                            </p>
-                                            <div
-                                                class="d-flex justify-content-between align-items-center"
-                                            >
-                                                <a
-                                                    href="single.html"
-                                                    class="btn btn-sm btn-dark"
-                                                    >مشاهده</a
-                                                >
-
-                                                <p class="fs-7 mb-0">
-                                                    نویسنده : علی شیخ
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="card">
-                                        <img
-                                            src="./assets/images/6.jpg"
-                                            class="card-img-top"
-                                            alt="post-image"
-                                        />
-                                        <div class="card-body">
-                                            <div
-                                                class="d-flex justify-content-between"
-                                            >
-                                                <h5 class="card-title fw-bold">
-                                                    لورم ایپسوم
-                                                </h5>
-                                                <div>
-                                                    <span
-                                                        class="badge text-bg-secondary"
-                                                        >متفزقه</span
-                                                    >
-                                                </div>
-                                            </div>
-                                            <p
-                                                class="card-text text-secondary pt-3"
-                                            >
-                                                لورم ایپسوم متن ساختگی با تولید
-                                                سادگی نامفهوم از صنعت چاپ و با
-                                                استفاده از طراحان گرافیک است.
-                                                چاپگرها و متون بلکه روزنامه و
-                                                مجله در ستون و سطرآنچنان که لازم
-                                                است و برای شرایط فعلی تکنولوژی
-                                                مورد نیاز و کاربردهای متنوع با
-                                                هدف بهبود
-                                            </p>
-                                            <div
-                                                class="d-flex justify-content-between align-items-center"
-                                            >
-                                                <a
-                                                    href="single.html"
-                                                    class="btn btn-sm btn-dark"
-                                                    >مشاهده</a
-                                                >
-
-                                                <p class="fs-7 mb-0">
-                                                    نویسنده : علی شیخ
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                                            
+                                    <?php endforeach; ?>
+                                            <?php 
+                                                }
+                                                else{
+                                            ?>
+                                                 <h2>هیچ محتوایی برای نمایش وجود ندارد</h2>
+                                            <?php }?>
                             </div>
                         </div>
 
