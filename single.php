36f9f4567dc38144d983425ccb966b5815930a49
Behzad <mirbehzad2004@gmail.com>
2025-02-05 13:55:00 +0330

    post style changed in single.php and comment section added to it

--- single.php
+++ single.php
@@ -27,9 +27,9 @@ include 'include/layout/header.php';
                     <div class="row">
                         <!-- Posts Content -->
                         <div class="col-lg-8">
-                            <div class="row g-3">
+                            <div class="row g-3 justify-content-center">
                                 
-                                            <div class="col-sm-6">
+                                            <div class="col-12 " >
                                                 <div class="card">
                                                     <img
                                                         src="./uploads/posts/<?= $post['image']; ?>"
@@ -53,18 +53,12 @@ include 'include/layout/header.php';
                                                         <p
                                                             class="card-text text-secondary pt-3"
                                                         >
-                                                            <?= substr($post['body'],0,200) . "..."; ?>
+                                                            <?= $post['body']; ?>
                                                         </p>
                                                         <div
                                                             class="d-flex justify-content-between align-items-center"
                                                         >
-                                                            <a
-                                                                href="single.php?postId= <?= $post['id']; ?>"
-                                                                class="btn btn-sm btn-dark"
-                                                                >مشاهده</a
-                                                            >
-
-                                                            <p class="fs-7 mb-0">
+                                                            <p class="fs-7 mb-0 mt-3">
                                                                 نویسنده :  <?= $post['author']; ?>
                                                             </p>
                                                         </div>
@@ -86,6 +80,128 @@ include 'include/layout/header.php';
                     </div>
                 </section>
             </main>
+
+            <!-- Comment Section -->
+            <div class="col-8 mt-4">
+                                    <!-- Comment Form -->
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <p class="fw-bold fs-5">
+                                                ارسال کامنت
+                                            </p>
+
+                                            <form>
+                                                <div class="mb-3">
+                                                    <label class="form-label"
+                                                        >نام</label
+                                                    >
+                                                    <input
+                                                        type="text"
+                                                        class="form-control"
+                                                    />
+                                                </div>
+                                                <div class="mb-3">
+                                                    <label class="form-label"
+                                                        >متن کامنت</label
+                                                    >
+                                                    <textarea
+                                                        class="form-control"
+                                                        rows="3"
+                                                    ></textarea>
+                                                </div>
+                                                <button
+                                                    type="submit"
+                                                    class="btn btn-dark"
+                                                >
+                                                    ارسال
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                    <hr class="mt-4" />
+                                    <!-- Comment Content -->
+                                    <p class="fw-bold fs-6">تعداد کامنت : 3</p>
+
+                                    <div class="card bg-light-subtle mb-3">
+                                        <div class="card-body">
+                                            <div
+                                                class="d-flex align-items-center"
+                                            >
+                                                <img
+                                                    src="./assets/images/profile.png"
+                                                    width="45"
+                                                    height="45"
+                                                    alt="user-profle"
+                                                />
+
+                                                <h5
+                                                    class="card-title me-2 mb-0"
+                                                >
+                                                    محمد صالحی
+                                                </h5>
+                                            </div>
+
+                                            <p class="card-text pt-3 pr-3">
+                                                لورم ایپسوم متن ساختگی با تولید
+                                                سادگی نامفهوم از صنعت چاپ و با
+                                                استفاده از طراحان گرافیک است.
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div class="card bg-light-subtle mb-3">
+                                        <div class="card-body">
+                                            <div
+                                                class="d-flex align-items-center"
+                                            >
+                                                <img
+                                                    src="./assets/images/profile.png"
+                                                    width="45"
+                                                    height="45"
+                                                    alt="user-profle"
+                                                />
+
+                                                <h5
+                                                    class="card-title me-2 mb-0"
+                                                >
+                                                    متین سیدی
+                                                </h5>
+                                            </div>
+
+                                            <p class="card-text pt-3 pr-3">
+                                                لورم ایپسوم متن ساختگی با تولید
+                                                سادگی نامفهوم از صنعت چاپ
+                                            </p>
+                                        </div>
+                                    </div>
+
+                                    <div class="card bg-light-subtle mb-3">
+                                        <div class="card-body">
+                                            <div
+                                                class="d-flex align-items-center"
+                                            >
+                                                <img
+                                                    src="./assets/images/profile.png"
+                                                    width="45"
+                                                    height="45"
+                                                    alt="user-profle"
+                                                />
+
+                                                <h5
+                                                    class="card-title me-2 mb-0"
+                                                >
+                                                    زهرا عزیزی
+                                                </h5>
+                                            </div>
+
+                                            <p class="card-text pt-3 pr-3">
+                                                لورم ایپسوم متن ساختگی با تولید
+                                                سادگی
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
 
             <!-- Footer Section -->
             <?php
